4f2b1aa0bb95e0394e07845ebe4782af75953ae5
codyfet <codyfet@mail.ru>
2019-03-18 15:25:30 +0300

    Добавил логику для создания vendor и common чанков

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var AsyncChunkNames = require('webpack-async-chunk-names-plugin');
 
 var htmlPlugin = new HtmlWebpackPlugin({
     template: "./src/index.html",
     filename: "./index.html"
 });
 
+var asyncChunkNames = new AsyncChunkNames();
+
 module.exports = {
     entry: './src/index.jsx',
     output: {
         path: __dirname + '/dist',
-        filename: 'main.js'
+        filename: 'main.js',
+        chunkFilename: '[name].js'
     },
     module: {
         rules: [
@@ -17,7 +21,7 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
-            }, 
+            },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
@@ -28,8 +32,38 @@ module.exports = {
             }
         ],
     },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                /**
+                 * Отключим дефолтные настройки.
+                 */
+                default: false,
+                vendors: false,
+                /**
+                 * Настраиваем вендор чанк.
+                 */
+                vendor: {
+                    // Имя вендор чанка.
+                    name: 'vendor',
+                    // Включить в вендор чанк синхронные и всинхронные модули.
+                    chunks: 'all',
+                    // Включить в вендор чанк модули из node_modules.
+                    test: /node_modules/
+                },
+                common: {
+                    name: 'common',
+                    minChunks: 2,
+                    chunks: 'async',
+                    priority: 10,
+                    reuseExistingChunk: true,
+                    enforce: true
+                }
+            }
+        }
+    },
     resolve: {
         extensions: ['.js', '.jsx', '.css', '.scss']
     },
-    plugins: [htmlPlugin]
+    plugins: [htmlPlugin, asyncChunkNames]
 };
